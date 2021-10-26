c83478ba5d5fc5c87152d74691814f8a4d391675
JoeGilliganHW <91262415+JoeGilliganHW@users.noreply.github.com>
2021-10-26 16:29:25 +0100

    stopped at window border

--- game.js
+++ game.js
@@ -52,3 +52,25 @@ function moveBear(e) {
     bear.move(0, 1);
   } // down key
 }
+
+this.fitBounds = function () {
+  let parent = this.htmlElement.parentElement;
+  let iw = this.htmlElement.offsetWidth;
+  let ih = this.htmlElement.offsetHeight;
+  let l = parent.offsetLeft;
+  let t = parent.offsetTop;
+  let w = parent.offsetWidth;
+  let h = parent.offsetHeight;
+
+  if (this.x < 0) this.x = 0;
+  if (this.x > w - iw) this.x = w - iw;
+  if (this.y < 0) this.y = 0;
+  if (this.y > h - ih) this.y = h - ih;
+};
+
+this.move = function (xDir, yDir) {
+  this.fitBounds(); //we add this instruction to keep bear within board
+  this.x += this.dBear * xDir;
+  this.y += this.dBear * yDir;
+  this.display();
+};
