fa924cf1cf7d4bbf3c2cc682b989534a37f4e611
JoeGilliganHW <91262415+JoeGilliganHW@users.noreply.github.com>
2021-11-02 17:40:19 +0000

    BEEEEEEEEEEEE

--- game.js
+++ game.js
@@ -49,8 +49,13 @@ function start() {
   bear = new Bear();
 
   //Add an event lsitener to the keypress event
-
   document.addEventListener("keydown", moveBear, false);
+
+  //create new array for bees
+  bees = new Array();
+
+  //create bees
+  makeBees();
 }
 
 // Handle keyboad events
@@ -119,5 +124,60 @@ class Bee {
 
       if (this.y > h - ih) this.y = h - ih;
     };
+  }
+}
+
+function createBeeImg(wNum) {
+  //get dimension and position of board div
+  let boardDiv = document.getElementById("board");
+  let boardDivW = boardDiv.offsetWidth;
+  let boardDivH = boardDiv.offsetHeight;
+  let boardDivX = boardDiv.offsetLeft;
+  let boardDivY = boardDiv.offsetTop;
+  //create the IMG element
+  let img = document.createElement("img");
+  img.setAttribute("src", "images/bee.gif");
+  img.setAttribute("width", "100");
+  img.setAttribute("alt", "A bee!");
+  img.setAttribute("id", "bee" + wNum);
+  img.setAttribute("class", "bee");
+  //set class of html tag img
+  //add the IMG element to the DOM as a child of the board div
+  img.style.position = "absolute";
+  boardDiv.appendChild(img);
+  //set initial position
+  let x = getRandomInt(boardDivW);
+  let y = getRandomInt(boardDivH);
+  img.style.left = boardDivX + x + "px";
+  img.style.top = y + "px";
+  //return the img object
+  return img;
+}
+
+function getRandomInt(max) {
+  return Math.random() * max;
+}
+
+function makeBees() {
+  //get number of bees specified by the user
+  let nbBees = document.getElementById("nbBees").value;
+
+  //try converting the content of the input to a number
+  nbBees = Number(nbBees);
+
+  //check that the input field contains a valid number
+  if (isNaN(nbBees)) {
+    window.alert("Invalid number of bees");
+    return;
+  }
+
+  //create bees
+  let i = 1;
+  while (i <= nbBees) {
+    var num = i;
+    var bee = new Bee(num); //create object and its IMG element
+    bee.display(); //display the bee
+    bees.push(bee); //add the bee object to the bees array
+    i++;
   }
 }
